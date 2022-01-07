37c4b1a2a29b181c7b7dfbdf242dd722d62b590c
elasticspoon <quesadillaman@gmail.com>
2022-01-07 13:25:13 -0500

    added posts controller actually

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  resources :users
-  root 'users#index'
+  resources :posts
+  root 'posts#index'
 end
